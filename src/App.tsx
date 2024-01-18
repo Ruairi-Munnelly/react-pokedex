d9ce5bc1c58ac845964845b51f10426d0eca3d56
Ruairi-Munnelly <40793154+Ruairi-Munnelly@users.noreply.github.com>
2024-01-18 16:03:53 +0000

    styling and layout updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,44 @@ import "./App.css";
 import Spinner from "./components/Spinner";
 
 function App() {
-
-  type PokeData ={
-    [key:string] : any;
-  }
+  type PokeData = {
+    [key: string]: any;
+  };
 
   const POKEDEX_ENDPOINT = "https://pokeapi.co/api/v2/pokemon?limit=151";
-  const { data, loading}: { data: PokeData, loading:boolean} =
+  const { data, loading }: { data: PokeData; loading: boolean } =
     useFetch(POKEDEX_ENDPOINT);
 
   const pokedex = data.results;
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App h-screen bg-slate-500">
+      <header className="App-header flex">
+        <div className="image-header flex-1 basis-1/2">
+          <img
+            className="relative m-auto"
+            src="/pokemon-logo.svg"
+            alt="Pokemon Logo"
+            width={360}
+            height={74}
+          />
+        </div>
       </header>
       <main>
         {loading ? (
           <div className="spinner-container h-screen flex justify-center items-center">
-          <Spinner size={240}/> 
-        </div>
+            <Spinner size={240} />
+          </div>
         ) : (
           <div className="pokedex">
             <PokemonContextProvider>
-              <div className="pokedex flex" style={{ width: "100%", margin: '0 50px'  }}>
+              <div className="pokedex flex w-full flex-wrap">
                 <PokeData />
                 <PokeSidebar pokedex={pokedex} />
               </div>
             </PokemonContextProvider>
           </div>
         )}
-      </main> 
+      </main>
     </div>
   );
 }
